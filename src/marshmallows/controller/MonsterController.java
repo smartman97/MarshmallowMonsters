941f6eed398b3c9773bc45744a73ef84041c55c2
smartman97 <smartman97@gmail.com>
2015-09-15 09:55:01 -0600

    Added the askQuestions method
    Made it so the user can change parts of the monster and it will show
    the new monster

--- src/marshmallows/controller/MonsterController.java
+++ src/marshmallows/controller/MonsterController.java
@@ -1,5 +1,6 @@
 package marshmallows.controller;
 
+import java.util.Scanner;
 import marshmallows.model.MarshmallowMonster;
 import marshmallows.view.MarshmallowOutput;
 
@@ -7,16 +8,18 @@ public class MonsterController
 {
 	private MarshmallowMonster colmMonster;
 	private MarshmallowOutput myOutput;
+	private Scanner monsterScanner;
 	
 	public MonsterController()
 	{
 		int eyes = 2;
+		int noses = 0;
 		double legs = 3;
 		double hair = 3;
-		int noses = 0;
 		boolean hasBellyButton = true;
 		String name = "Jar Jar";
 		
+		monsterScanner = new Scanner(System.in);
 		colmMonster = new MarshmallowMonster(name, eyes, noses, hair, legs, hasBellyButton);
 		myOutput = new MarshmallowOutput();
 	}
@@ -24,6 +27,35 @@ public class MonsterController
 	public void start()
 	{
 		myOutput.displayMonsterInfo(colmMonster.toString());
+		askQuestions();
+		myOutput.displayMonsterInfo("New Monster Info " + colmMonster.toString());
 	}
 	
+	private void askQuestions()
+	{
+		System.out.println("Type in a better name for the monster ");
+		String betterMonsterName = monsterScanner.next();
+		colmMonster.setMonsterName(betterMonsterName);
+		
+		System.out.println("Type in a new number of eyes for the monster");
+		int betterMonsterEyes = monsterScanner.nextInt();
+		colmMonster.setMonsterEyes(betterMonsterEyes);
+		
+		System.out.println("Type in a new number of Noses for the monster");
+		int betterMonsterNoses = monsterScanner.nextInt();
+		colmMonster.setMonsterNoses(betterMonsterNoses);
+		
+		System.out.println("Type in a new number of Legs for the monster");
+		double betterMonsterLegs = monsterScanner.nextDouble();
+		colmMonster.setMonsterLegs(betterMonsterLegs);
+		
+		System.out.println("Type in a new number of Hairs for the monster");
+		double betterMonsterHair = monsterScanner.nextDouble();
+		colmMonster.setMonsterHair(betterMonsterHair);
+		
+		System.out.println("Type in whether it is true or false for the monster to have a belly button");
+		boolean betterMonsterBellyButton = monsterScanner.nextBoolean();
+		colmMonster.setMonsterBellyButton(betterMonsterBellyButton);
+
+	}
 }
