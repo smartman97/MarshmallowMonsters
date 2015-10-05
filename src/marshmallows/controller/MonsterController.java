cd07c89a26320bba910388e30e552ecdc48e2259
smartman97 <smartman97@gmail.com>
2015-10-05 09:32:16 -0600

    Created pop ups
    added a method so that the user now inputs via pop ups.

--- src/marshmallows/controller/MonsterController.java
+++ src/marshmallows/controller/MonsterController.java
@@ -1,7 +1,9 @@
 package marshmallows.controller;
 
 import java.util.Scanner;
+
 import marshmallows.model.MarshmallowMonster;
+import marshmallows.view.MarshmallowDisplay;
 import marshmallows.view.MarshmallowOutput;
 
 public class MonsterController
@@ -10,6 +12,7 @@ public class MonsterController
 	private MarshmallowMonster userMonster;
 	private MarshmallowOutput myOutput;
 	private Scanner monsterScanner;
+	private MarshmallowDisplay myPopups;
 	
 	public MonsterController()
 	{
@@ -23,11 +26,14 @@ public class MonsterController
 		monsterScanner = new Scanner(System.in);
 		colmMonster = new MarshmallowMonster(name, eyes, noses, hair, legs, hasBellyButton);
 		myOutput = new MarshmallowOutput();
+		myPopups = new MarshmallowDisplay();
 	}
 	
 	public void start()
 	{
-		myOutput.displayMonsterConsole(colmMonster.toString());
+		popupMonster();
+		
+		/*myOutput.displayMonsterConsole(colmMonster.toString());
 		myOutput.displayMonsterGUI(colmMonster.toString());
 		int count = 0;
 		while(count < 10)
@@ -35,7 +41,7 @@ public class MonsterController
 			myOutput.displayPopUpInfo(colmMonster.toString());
 			count++;
 		}
-		
+		*/
 		//makeUserMonster();
 		//myOutput.displayMonsterConsole(userMonster.toString());
 	}
@@ -102,6 +108,37 @@ public class MonsterController
 		
 		//Step three: Make a monster - use the Constructor!!!
 		userMonster = new MarshmallowMonster(userName, userEyes, userNoses, userHair, userLegs, userBellyButton);
+		
+	}
+	
+	private void popupMonster()
+	{
+		myPopups.showResponse(colmMonster.toString());
+		
+		String userName = myPopups.grabAnswer("Type in a monster name!");
+		myPopups.showResponse("The monsters name is: " + userName);
+		
+		String Eyes = myPopups.grabAnswer("How many eyes does it have?");
+		int userEyes = Integer.parseInt(Eyes);
+		myPopups.showResponse("It has " + userEyes + " eyes.");
+		
+		String Noses = myPopups.grabAnswer("How many Noses does it have?");
+		int userNoses = Integer.parseInt(Noses);
+		myPopups.showResponse("It has " + userNoses + " Noses.");
+		
+		String Hair = myPopups.grabAnswer("How much hair does it have?");
+		double userHair = Double.parseDouble(Hair);
+		myPopups.showResponse("It has " + userHair + " hairs.");
+		
+		String Legs = myPopups.grabAnswer("How many legs does it have?");
+		double userLegs = Double.parseDouble(Legs);
+		myPopups.showResponse("It has " + userLegs + " legs.");
+		
+		String bellyButton = myPopups.grabAnswer("Does it have a belly button? Type true or false");
+		boolean userBellyButton = Boolean.parseBoolean(bellyButton);
+		myPopups.showResponse("It is " + userBellyButton + " that it has a belly button.");
+		
+		
 		
 	}
 }
---
+++ src/marshmallows/view/MarshmallowDisplay.java
@@ -0,0 +1,33 @@
+package marshmallows.view;
+
+import javax.swing.JOptionPane;
+/**
+ * A popup display class
+ * @author Colm Laro
+ * @version 1 10/5/15
+ */
+public class MarshmallowDisplay
+{
+	/**
+	 * Displays the supplied text as a popup window
+	 * @param wordsFromSomewhere The text to be displayed.
+	 */
+	public void showResponse(String wordsFromSomewhere)
+	{
+		JOptionPane.showMessageDialog(null, wordsFromSomewhere);
+	}
+	/**
+	 * Displays a popup with a field to type in a response.
+	 * It returns the answer to the supplied question as a String.
+	 * @param stuff A question to be displayed in the popup window.
+	 * @return The user's input as a String data type.
+	 */
+	public String grabAnswer(String stuff)
+	{
+		String answer = "";
+		
+		answer = JOptionPane.showInputDialog(null, stuff);
+		
+		return answer;
+	}
+}
